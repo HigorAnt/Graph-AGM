a809664733b33ae0f1783a9cfaff7f8d78f5ea7a
Gabriela Ximenes <gabriela.ufc30@gmail.com>
2026-06-21 20:52:15 -0300

    alteração em prim

--- prim.hpp
+++ prim.hpp
@@ -2,74 +2,58 @@
 
 #include <vector>
 #include <queue>
-
 #include "graph.hpp"
 
 class Prim
 {
 public:
-
-    static std::pair<double, std::vector<Edge>>
-    run(const Graph& g)
+    static std::pair<double, std::vector<Edge>> run(const Graph& g)
     {
-        std::vector<std::vector<std::pair<int,double>>> adj(g.V);
+        if (g.V == 0) return {0.0, {}};
 
+        std::vector<std::vector<std::pair<int, double>>> adj(g.V);
         for(const auto& e : g.edges)
         {
-            adj[e.from].push_back(
-                {e.to, e.weight}
-            );
-
-            adj[e.to].push_back(
-                {e.from, e.weight}
-            );
+            adj[e.from].push_back({e.to, e.weight});
+            adj[e.to].push_back({e.from, e.weight});
         }
 
-        std::vector<bool>
-            inMST(g.V, false);
+        std::vector<bool> inMST(g.V, false);
+        
+        typedef std::pair<double, std::pair<int, int>> Node;
+        std::priority_queue<Node, std::vector<Node>, std::greater<Node>> pq;
 
-        std::priority_queue<
-            std::pair<double,int>,
-            std::vector<
-                std::pair<double,int>
-            >,
-            std::greater<
-                std::pair<double,int>
-            >
-        > pq;
-
-        pq.push({0,0});
+        pq.push({0.0, {0, -1}});
 
         double totalWeight = 0;
-
         std::vector<Edge> mst;
 
         while(!pq.empty())
         {
-            auto [weight, u] = pq.top();
-
+            auto [weight, nodeData] = pq.top();
+            int u = nodeData.first;
+            int parent = nodeData.second;
             pq.pop();
 
-            if(inMST[u])
-                continue;
+            if(inMST[u]) continue;
 
             inMST[u] = true;
-
             totalWeight += weight;
 
-            for(const auto&
-                [v, w] : adj[u])
+            if(parent != -1)
+            {
+                mst.push_back({parent, u, weight});
+            }
+
+            for(const auto& [v, w] : adj[u])
             {
                 if(!inMST[v])
                 {
-                    pq.push({w,v});
+                    pq.push({w, {v, u}});
                 }
             }
         }
 
-        return {
-            totalWeight,
-            mst
-        };
+        return {totalWeight, mst};
     }
 };
